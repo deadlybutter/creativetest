280569352b1d3880284bfa3f0e54a3d347ef9ced
deadlybutter <thedeadlybutter@gmail.com>
2016-02-06 17:38:07 -0500

    Default stats should be FPS

--- public/prototype.js
+++ public/prototype.js
@@ -158,7 +158,7 @@ function render() {
 $(document).on('ready', function() {
 
   stats = new Stats();
-  stats.setMode(1);
+  stats.setMode(0);
   stats.domElement.style.position = 'absolute';
   stats.domElement.style.left = '0px';
   stats.domElement.style.top = '0px';
