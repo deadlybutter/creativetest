1043fc64454985c029cecfc242ccfc7766f89f46
Joe Kent <thedeadlybutter@gmail.com>
2016-02-06 19:17:28 -0500

    Updates generator

--- generator.js
+++ generator.js
@@ -3,173 +3,77 @@ var secrets = require(__dirname + '/secrets.json');
 
 var mongoose = require('mongoose');
 mongoose.connect(secrets.mongo_path);
+mongoose.connection.on("error", function(err) {
+  console.log("Could not connect to mongo server!");
+  console.log(err);
+});
+mongoose.connection.on("open", function(ref) {
+  console.log("Connected to mongo server.");
+  console.log("Spinning up the voxel generator....");
+  makeAllChunks();
+});
+
 var Schema = mongoose.Schema;
 var schemas = tools.getMongoSchema(Schema, mongoose);
 var Chunk = schemas[0];
-var VerticeList = schemas[1];
-var ColorList = schemas[2];
 
-var tree = {};
-var queue = [];
+var queueB = [];
 
 var cl = 8;
 var ml = (process.argv[2] || 4) * cl;
 console.log("Chunk size is " + cl);
 console.log("Map size is " + ml);
 
-function getVoxelAt(x, y, z) {
-  var cc = tools.getChunkCoords(x, y, z, cl);
-  var block = {d: 0, c: 0};
-  try {
-    block = tree[cc[0]][cc[1]][cc[2]][x][y][z];
-  } catch(e) {}
-  return block;
-}
-
-function buildMap() {
-  // Create chunks
+function makeAllChunks() {
   for (var cx = 0; cx < ml; cx += cl) {
-    tree[cx] = {};
     for (var cy = 0; cy< ml; cy += cl) {
-      tree[cx][cy] = {};
       for (var cz = 0; cz < ml; cz += cl) {
-        // Chunk container
-        tree[cx][cy][cz] = {};
-        queue.push([cx, cy, cz]);
-
-        // Create blocks
-        for (var x = cx; x < (cx + cl); x++) {
-          tree[cx][cy][cz][x] = {};
-          for (var y = cy; y < (cy + cl); y++) {
-            tree[cx][cy][cz][x][y] = {};
-            for (var z = cz; z < (cz + cl); z++) {
-              // Add to world tree
-              var block = {
-                c: [tools.getRandomInt(50, 200), tools.getRandomInt(50, 200), tools.getRandomInt(50, 200)],
-                d: Math.random() >= 0.5 ? 1 : 0
-              };
-              tree[cx][cy][cz][x][y][z] = block;
-            }
-          }
-        }
+        queueB.unshift({x: cx, y: cy, z: cz});
       }
     }
-  }
 
-  console.log("Done building");
+    if (cx == 0) {
+      for (var i = 0; i < 3; i++) {
+        makeChunk(queueB.pop());
+      }
+    }
+
+    console.log(cx + ' ... ' + ml);
+  }
 }
 
-function createVertexData(cx, cy, cz) {
-  console.log("Creating vertex data for " + cx + ", " + cy + ", " + cz);
-
-  // Prepare buffers
-  var vertexPositions = [];
-  var colorVals = [];
+function makeChunk(pos) {
+  if (pos == undefined) {
+    return;
+  }
+  var cx = pos.x;
+  var cy = pos.y;
+  var cz = pos.z;
+  var path = tools.createChunkPath(cx, cy, cz);
+  var blocks = {};
 
   for (var x = cx; x < (cx + cl); x++) {
+    blocks[x] = {};
     for (var y = cy; y < (cy + cl); y++) {
+      blocks[x][y] = {};
       for (var z = cz; z < (cz + cl); z++) {
-
-        var block = tree[cx][cy][cz][x][y][z];
-        var localVertexPositions = [];
-
-        if (getVoxelAt(x, y + 1, z).d < block.d) {
-          localVertexPositions = localVertexPositions.concat(tools.cubeTop);
-        }
-
-        if (getVoxelAt(x, y - 1, z).d < block.d) {
-          localVertexPositions = localVertexPositions.concat(tools.cubeBottom);
-        }
-
-        if (getVoxelAt(x, y, z + 1).d < block.d) {
-          localVertexPositions = localVertexPositions.concat(tools.cubeFront);
-        }
-
-        if (getVoxelAt(x, y, z - 1).d < block.d) {
-          localVertexPositions = localVertexPositions.concat(tools.cubeBack);
-        }
-
-        if (getVoxelAt(x + 1, y, z).d < block.d) {
-          localVertexPositions = localVertexPositions.concat(tools.cubeRight);
-        }
-
-        if (getVoxelAt(x - 1, y, z).d < block.d) {
-          localVertexPositions = localVertexPositions.concat(tools.cubeLeft);
-        }
-
-        if (localVertexPositions.length == 0) {
-          continue;
-        }
-
-        localVertexPositions.forEach(function(v) {
-          v = v.slice();
-          v[0] += (x - cx);
-          v[1] += (y - cy);
-          v[2] += (z - cz);
-          vertexPositions.push(v);
-          colorVals.push(block.c);
-        });
-
+        // Add to world tree
+        blocks[x][y][z] = {
+          c: [tools.getRandomInt(50, 200), tools.getRandomInt(50, 200), tools.getRandomInt(50, 200)],
+          d: Math.random() >= 0.5 ? 1 : 0
+        };
       }
     }
   }
-
-  // Do vertices caluclations
-  var vertices = [];
-  var colors = [];
-
-  // components of the position vector for each vertex are stored
-  // contiguously in the buffer.
-  for (var i = 0; i < vertexPositions.length; i++) {
-    vertices[i * 3 + 0] = vertexPositions[i][0];
-    vertices[i * 3 + 1] = vertexPositions[i][1];
-    vertices[i * 3 + 2] = vertexPositions[i][2];
-
-    colors[i * 3 + 0] = colorVals[i][0];
-    colors[i * 3 + 1] = colorVals[i][1];
-    colors[i * 3 + 2] = colorVals[i][2];
-  }
-
-  var path = tools.createChunkPath(cx, cy, cz);
-  console.log("Done building vertex data");//vertices: vertices, colors: colors
-
-  var mongoChunk = new Chunk({chunkKey: path, blocks: tree[cx][cy][cz]});
-  mongoChunk.save(function(err) {
+  // console.log(blocks);
+  var mongoChunk = new Chunk({chunkKey: path, blocks: blocks});
+  mongoChunk.save(function(err, obj) {
     if (err) {
-      console.log(cx + "," + cy + "," + cz + " blocks");
       console.log(err);
-      return;
     }
-
-    var mongoVertices = new VerticeList({chunkKey: path, vertices: vertices});
-    mongoVertices.save(function(err) {
-      if (err) {
-        console.log(cx + "," + cy + "," + cz + " vertices");
-        console.log(err);
-        return;
-      }
-
-      var mongoColors = new ColorList({chunkKey: path, colors: colors});
-      mongoColors.save(function(err) {
-        if (err) {
-          console.log(cx + "," + cy + "," + cz + " colors");
-          console.log(err);
-          return;
-        }
-
-        console.log("All data saved, " + queue.length + " chunks left");
-        var nextChunk = queue.pop();
-        if (nextChunk != undefined) {
-          createVertexData(nextChunk[0], nextChunk[1], nextChunk[2]);
-        }
-        else {
-          console.log("DONE!");
-        }
-      });
-
-    });
+    else {
+      console.log(queueB.length + " chunks left");
+      makeChunk(queueB.pop())
+    }
   });
 }
-
-console.log("Spinning up the voxel generator....");
-buildMap();
